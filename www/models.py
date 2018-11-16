affee493ad1c5c2ccd1eb63510284a958ce6f76e
e <yueqirdt@163.com>
2018-11-16 15:01:15 +0800

    add models.py

---
+++ www/models.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'Michael Liao'
+
+import time, uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+def next_id():
+	 # uuid4()以随机方式生成uuid,hex属性将uuid转为32位的16进制数
+	 time.time()#%015d意思是打印结果为15位整数，当整数的位数不够15位时，在整数左侧补0，uuid.uuid4() : 基于随机数hex() 函数用于将10进制整数转换成16进制，以字符串形式表示。
+    # 返回当前时间的时间戳（1970纪元后经过的浮点秒数）。
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
