dc233af8036792f09f05ac62a30968a5f6ae3afd
e <yueqirdt@163.com>
2018-11-30 09:33:49 +0800

    modified handlers.py

--- www/handlers.py
+++ www/handlers.py
@@ -4,3 +4,16 @@
 __author__ = 'Michael Liao'
 
 ' url handlers '
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+def index(request):
+    users = yield from User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
